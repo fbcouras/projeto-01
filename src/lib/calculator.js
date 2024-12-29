1f0f9e7f6c0d700673589a0bf2dd8d2038cfa99e
fbcouras <fbcouras@gmail.com>
2024-12-29 15:25:36 -0300

    Adiciona projeto de calculadora com seus testes

---
+++ src/lib/calculator.js
@@ -0,0 +1,12 @@
+const { parse } = require('path');
+
+module.exports.sum = (a, b) => {
+  const num1 = parseInt(a, 10);
+  const num2 = parseInt(b, 10);
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    throw new Error('Parameters must be numbers');
+  }
+
+  return +a + +b;
+};
